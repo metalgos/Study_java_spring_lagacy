0e41450ef691033dbbad73d6688b2ccc17b5cea0
kyscodeman <metalgos2@gmail.com>
2023-08-02 00:35:38 +0900

    스프링 xml 지시서 사용법

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,5 +1,9 @@
 package spring.di;
 
+import org.omg.CORBA.portable.ApplicationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.Exam;
 import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsle;
@@ -11,14 +15,15 @@ public class Program {
 	public static void main(String[] args) {
 
 		/*
-		 * Exam exam = new NewlecExam(); 
-		 * ExamConsle console = new GridExamConsole();
+		 * Exam exam = new NewlecExam(); ExamConsle console = new GridExamConsole();
 		 * 
 		 * console.setExam(exam);
 		 */
-		
-		
-		ExamConsle console = ?;
+		ApplicationContext context
+		= new ClassPathXmlApplicationContext("spring/di/setting.xml");
+
+	ExamConsle console = (ExamConsle) context.getBean("console");
+		//ExamConsle console = context.getBean(ExamConsle.class);
 		console.print();
 
 	}
