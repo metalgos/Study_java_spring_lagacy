fe14eae692b20dfe486f2dd57ccfe7f4903d0046
kyscodeman <metalgos2@gmail.com>
2023-08-06 18:58:02 +0900

    스프링 autowired xml 설정과 중복일떄 qulifier 어노테이션 공부

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -23,20 +23,20 @@ public class Program {
 		 * console.setExam(exam);
 		 */
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
-		Exam exam = context.getBean(Exam.class);
-		System.out.println(exam.toString());
+		//Exam exam = context.getBean(Exam.class);
+		//System.out.println(exam.toString());
 		ExamConsle console = (ExamConsle) context.getBean("console");
 		// ExamConsle console = context.getBean(ExamConsle.class);
 		console.print();
 		
-		List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
+		//List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
 		//exams.add(new NewlecExam(1,1,1,1));
 		
 		//new ArrayList<E>();
 		 
-		for(Exam e : exams) {
-			System.out.println(e);
-		}
+		//for(Exam e : exams) {
+		//	System.out.println(e);
+		//}
 		
 		
 
--- spring/src/spring/di/ui/InlineExamConsole.java
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -1,5 +1,8 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsle {
@@ -17,6 +20,9 @@ public InlineExamConsole() {
 	public void print() {
 System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
+	
+	@Autowired
+	@Qualifier("exam2") //xml에서  id가 exam1으로 설정된것을 주입
 	public void setExam(Exam exam) {
 		this.exam = exam;
 		
