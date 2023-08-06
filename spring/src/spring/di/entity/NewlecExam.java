ee98577c67fc181597e0d927f7e93706fb7c8f4e
kyscodeman <metalgos2@gmail.com>
2023-08-06 23:08:35 +0900

    xml 에서 자바 파일 설정으로변경

--- spring/src/spring/di/entity/NewlecExam.java
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -1,11 +1,15 @@
 package spring.di.entity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+
 public class NewlecExam implements Exam {
 	
+	@Value("20")
 	private int kor;
+	@Value("30")
 	private int eng;
 	private int math;
 	private int com;
