414e15ef2b776f5e0313b29635347bb27c32039f
kyscodeman <metalgos2@gmail.com>
2023-08-06 18:59:10 +0900

    222

--- spring/src/spring/di/ui/InlineExamConsole.java
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -19,8 +19,7 @@ public InlineExamConsole() {
 	@Override
 	public void print() {
 System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
-	}
-	
+	}	
 	@Autowired
 	@Qualifier("exam2") //xml에서  id가 exam1으로 설정된것을 주입
 	public void setExam(Exam exam) {
