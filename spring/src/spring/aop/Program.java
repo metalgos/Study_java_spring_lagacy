c849d4a0661798312c88f36bba90e46745dab9d0
kyscodeman <metalgos2@gmail.com>
2023-08-10 01:25:17 +0900

    포인트컷 공부

--- spring/src/spring/aop/Program.java
+++ spring/src/spring/aop/Program.java
@@ -21,7 +21,6 @@ public class Program {
 		
 		System.out.printf("total is %d\n" , exam.total());
 		System.out.printf("avg is %f\n" , exam.avg());
-		
 		/*
 		Exam exam = new NewlecExam(1,1,1,1);
 	
